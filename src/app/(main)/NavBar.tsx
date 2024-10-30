88a66eef1e9cdc8bb136c2f4e192382115398c53
Rafael Schranz <106320220+rafaelschranz@users.noreply.github.com>
2024-10-30 14:18:07 +0100

    changed navbar name to rafael's analytics

--- src/app/(main)/NavBar.tsx
+++ src/app/(main)/NavBar.tsx
@@ -84,7 +84,7 @@ export function NavBar() {
         <Icon size="lg">
           <Icons.Logo />
         </Icon>
-        <Text>umami</Text>
+        <Text>Rafael's Analytics</Text>
       </div>
       <div className={styles.links}>
         {links.map(({ url, label }) => {

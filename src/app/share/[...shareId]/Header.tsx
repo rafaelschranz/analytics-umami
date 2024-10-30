3dc02aa85641bf77fc2b8f0f4ca485966cacd24e
Rafael Schranz <106320220+rafaelschranz@users.noreply.github.com>
2024-10-30 14:09:16 +0100

    changed name and logo to rafael's

--- src/app/share/[...shareId]/Header.tsx
+++ src/app/share/[...shareId]/Header.tsx
@@ -14,7 +14,7 @@ export function Header() {
           <Icon size="lg">
             <Icons.Logo />
           </Icon>
-          <Text>umami</Text>
+          <Text>Rafael's Analytics</Text>
         </Link>
       </div>
       <div className={styles.buttons}>
